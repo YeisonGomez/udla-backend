ea64c1a2fcaec5b80188952b799d031426951c4b
root <yeigomez97@hotmail.com>
2016-10-23 00:51:55 -0500

    quit image

--- config.js
+++ config.js
@@ -1,14 +1,13 @@
 module.exports = {
 	server 	: {
-		ip 		: '104.236.225.92',
-		//ip: 'localhost',
+		//ip 		: '104.236.225.92',
+		ip: 'localhost',
 		port    : 3200
 	},
 	
 	db : {
-		//host 		: '127.0.0.1',
-		host: 'localhost',
-	    user 		: 'root',
+		host 		: '104.236.225.92',
+	    user 		: 'yeigomez',
 	    password 	: 'persian13',
 	    port 		: '3306', 
     	database 	: 'myapp'
